a2a01066924fbec9fed4e7159b8199be290d7ae2
jnguyen7410 <jurytann@grubhub.com>
2021-12-31 17:58:40 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jurytan Nguyen on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
